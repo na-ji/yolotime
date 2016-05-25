1e6d9e4d2b872f1a279694a1f3d65775f805a378
Naji Astier <naji.demolitionman@gmail.com>
2016-05-25 15:32:37 -0700

    Add wakatime login

---
+++ server/accounts.js
@@ -0,0 +1,26 @@
+Accounts.onCreateUser(function(options, user) {
+	if (options.profile) {
+		user.profile = options.profile;
+	} else {
+		user.profile = {};
+	}
+
+	if (user.services['wakatime'] !== undefined) {
+		user.profile.name     = user.services.wakatime.full_name;
+		user.profile.username = user.services.wakatime.username;
+		user.username         = user.services.wakatime.username;
+		user.profile.avatar   = user.services.wakatime.photo;
+		user.uid              = user.services.wakatime.id;
+
+		if (user.emails === undefined) {
+			user.emails = [{
+				"address"  : user.services.wakatime.email,
+				"verified" : true
+			}];
+		}
+	}
+
+	console.log(user);
+
+	return user;
+});
